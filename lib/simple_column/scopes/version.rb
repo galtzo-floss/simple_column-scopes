50139e6e6b89b11d4cd8179f365543fa362c9ee7
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 00:18:14 -0600

    🛠️ Restore Scopes class API after templating

--- lib/simple_column/scopes/version.rb
+++ lib/simple_column/scopes/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SimpleColumn
-  module Scopes
+  class Scopes < Module
     # Version namespace for this gem.
     module Version
       # Current gem version.
