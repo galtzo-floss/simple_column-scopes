c9336b697a47f747f38b16bc72a268625b3d9f15
Peter H. Boling <peter.boling@gmail.com>
2026-08-04 14:40:38 -0600

    Fix templated class-based scopes namespace

--- lib/simple_column/scopes/version.rb
+++ lib/simple_column/scopes/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SimpleColumn
-  class Scopes
+  class Scopes < Module
     # Version namespace for this gem.
     module Version
       # Current gem version.
