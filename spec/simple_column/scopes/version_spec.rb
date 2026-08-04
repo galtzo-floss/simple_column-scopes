6ba730a05df215534f2f9d88b26cd22158ce4bd3
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 19:30:25 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/simple_column/scopes/version_spec.rb
+++ spec/simple_column/scopes/version_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 require "anonymous_loader"
-require "simple_column-scopes"
+require "simple_column/scopes"
 
 RSpec.describe SimpleColumn::Scopes::Version do
   it_behaves_like "a Version module", described_class
