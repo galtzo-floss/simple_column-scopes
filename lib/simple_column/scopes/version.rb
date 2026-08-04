f97c65fbcef88e25041ea692fc9aee3487693bef
Peter H. Boling <peter.boling@gmail.com>
2026-08-04 02:59:28 -0600

    Restore simple column scopes class namespace

--- lib/simple_column/scopes/version.rb
+++ lib/simple_column/scopes/version.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 module SimpleColumn
-  module Scopes
-    # Version namespace for this gem.
+  # This is a Class / Module Hybrid (see simple_column/scopes.rb)
+  class Scopes < Module
     module Version
-      # Current gem version.
       VERSION = "0.1.1"
     end
-    # Current gem version exposed at the traditional constant location.
-    VERSION = Version::VERSION # Traditional Constant Location
+    VERSION = Version::VERSION # Traditional version location
   end
 end
