adcf9df8f1c6a3ec9e96fec3a58cf1c95c8a8f52
Peter H. Boling <peter.boling@gmail.com>
2026-08-08 21:07:20 -0600

    🔖 Bump gem version

--- lib/simple_column/scopes/version.rb
+++ lib/simple_column/scopes/version.rb
@@ -5,7 +5,7 @@ module SimpleColumn
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = "0.1.1"
+      VERSION = "0.1.2"
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
